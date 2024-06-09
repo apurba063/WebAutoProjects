aefda8a47ab67287534f39960510bcf04483431c
Apurba Roy Ajay <98884327+apurba063@users.noreply.github.com>
2024-06-10 04:07:12 +0600

    add to cart complete

--- src/test/java/TestCase/TestCaseBata.java
+++ src/test/java/TestCase/TestCaseBata.java
@@ -1,4 +1,111 @@
 package TestCase;
 
-public class TestCaseBata {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.SignInPage;
+import utilities.BrowserSetup;
+
+import javax.swing.*;
+
+
+public class TestCaseBata extends BrowserSetup {
+        HomePage homePage = new HomePage();
+        SignInPage signInPage = new SignInPage();
+
+        @BeforeMethod
+        public void loadHomePage() throws InterruptedException {
+            getBrowser().get(homePage.url);
+            homePage.clickOnElement(homePage.signInButton);
+            Thread.sleep(1000);
+        }
+        @Test
+        public void LogInIntoBata() throws InterruptedException {
+            signInPage.writeOnElement(signInPage.emailInputBox, "cahev71384@eqvox.com");
+            signInPage.writeOnElement(signInPage.passwordInputBox, "@A123456a@");
+            signInPage.clickOnElement(signInPage.logInButton);
+            Assert.assertTrue(homePage.displayStatus(homePage.Assertcheck));
+            Thread.sleep(1000);
+
+
+            //hover
+            Actions actions = new Actions(getBrowser());
+            WebElement hoverelement = homePage.getElement(By.xpath("//body/div[@class='wrapper-header']/div[@id='shopify-section-navigation']/div[@id='sticky-wrapper']/div[@class='wrapper-navigation']/div[@class='main-menu jas-mb-style']/div[@class='container']/div[@class='row']/div[@class='col-12']/div[@class='mb-area']/nav[@role='navigation']/ul[@class='site-nav']/li[3]/a[1]/span[1]"));
+            actions.clickAndHold(hoverelement).build().perform();
+            Thread.sleep(1000);
+            signInPage.clickOnElement(By.cssSelector("body > div:nth-child(3) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > nav:nth-child(1) > ul:nth-child(1) > li:nth-child(3) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(3) > a:nth-child(1) > span:nth-child(1)"));
+            Thread.sleep(1000);
+            signInPage.clickOnElement(By.cssSelector("body > div:nth-child(3) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > nav:nth-child(1) > ul:nth-child(1) > li:nth-child(3) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(3) > a:nth-child(1) > span:nth-child(1)"));
+            Thread.sleep(1000);
+
+            //scroll to another page
+            JavascriptExecutor js = (JavascriptExecutor)getBrowser();
+            WebElement Scroll = homePage.getElement(By.xpath("//label[normalize-space()='11']"));
+            js.executeScript("arguments[0].scrollIntoView();",Scroll);
+            Thread.sleep(1000);
+            homePage.clickOnElement(homePage.scroll2);
+            Thread.sleep(1000);
+
+            //scroll on the 2nd page
+            JavascriptExecutor js2 = (JavascriptExecutor)getBrowser();
+            WebElement Scroll2 = homePage.getElement(By.cssSelector(".lazyautosizes.lazyloaded[data-srcset='//www.batabd.com/cdn/shop/files/2_fb5b0abb-db59-44e5-b1ce-daff59b9ebe3_300x.jpg?v=1712209772']"));
+            js2.executeScript("arguments[0].scrollIntoView();",Scroll2);
+            Thread.sleep(5000);
+
+            //2nd page mouse hover and click
+            Actions actions2 = new Actions(getBrowser());
+            WebElement hoverelement2 = homePage.getElement(By.cssSelector(".lazyautosizes.lazyloaded[data-srcset='//www.batabd.com/cdn/shop/files/2_fb5b0abb-db59-44e5-b1ce-daff59b9ebe3_300x.jpg?v=1712209772']"));
+            actions.clickAndHold(hoverelement2).build().perform();
+            Thread.sleep(2000);
+            signInPage.clickOnElement(By.xpath("//div[@class='abs-bottom-mb']//form[@id='-7221917483091']"));
+            Thread.sleep(2000);
+            signInPage.clickOnElement(By.xpath("//div[@class='abs-bottom-mb']//form[@id='-7221917483091']"));
+            Thread.sleep(2000);
+
+            //scroll and add to cart
+            JavascriptExecutor js3 = (JavascriptExecutor)getBrowser();
+            WebElement Scroll3 = homePage.getElement(By.xpath("//form[@id='add-to-cart-form']//input[@name='quantity']"));
+            js.executeScript("arguments[0].scrollIntoView();",Scroll3);
+            Thread.sleep(1000);
+            homePage.clickOnElement(homePage.scroll3);
+            Thread.sleep(1000);
+            homePage.clickOnElement(homePage.scroll3);
+            Thread.sleep(1000);
+
+            //scroll to go add to cart and clicl
+            JavascriptExecutor js4 = (JavascriptExecutor)getBrowser();
+            WebElement Scroll4 = homePage.getElement(By.xpath("//div[@class='nav-search']//input[@placeholder='SEARCH']"));
+            js.executeScript("arguments[0].scrollIntoView();",Scroll4);
+            Thread.sleep(1000);
+
+            Actions actions3 = new Actions(getBrowser());
+            WebElement hoverelement3 = homePage.getElement(By.xpath("//div[@class='wrapper-top-cart cart-icon']//span[@class='cartCount'][normalize-space()='2']"));
+            actions.clickAndHold(hoverelement3).build().perform();
+            Thread.sleep(6000);
+            homePage.clickOnElement(homePage.scroll4);
+            Thread.sleep(1000);
+            homePage.clickOnElement(homePage.scroll4);
+            Thread.sleep(1000);
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
 }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class HomePage extends BasePage{
     public String url = "https://www.batabd.com/";
@@ -11,4 +12,12 @@ public class HomePage extends BasePage{
     public By errorMsgforemail = By.cssSelector(".error_text");
     public By errorMsgforrequiredata = By.xpath("//span[@class='error_pass_text']");
     public By Assertcheck = By.xpath("//button[normalize-space()='My Profile']");
+    public By menuhover = By.xpath("//a[@href='https://www.batabd.com/collections/men?constraint=bata']//span[contains(text(),'Bata')]");
+    public By Scroll = (By.xpath("//a[normalize-space()='2']"));
+
+    public By scroll2 =(By.xpath("//div[@class='product-pagination has-pagination']//li[3]"));
+
+    public By scroll3 =(By.xpath("//input[@id='product-add-to-cart']"));
+    public By scroll4 =(By.xpath("//button[normalize-space()='Check out now']"));
+
 }
